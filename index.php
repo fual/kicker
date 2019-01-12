af9e793c26b63638ed4ccbca1e8ca97f5e5d6a60
Aleksander Antropov <aleksander.antropov@gmail.com>
2019-01-12 12:27:00 +0300

    Update all

--- index.php
+++ index.php
@@ -30,59 +30,83 @@
 		$teams_quantity = $sth->fetch()['number'];
 		/* teams names ordered by points scored */
 		$sth = $db->prepare("
-				select t.team_name_short as name from teams as t
-				inner join matches as m on t.team_id = m.team_id1
+				with points_table as
+				(select
+				ht.team_name_short as name,
+				sum(m.points_1) as points,
+				count(*) as games_played
+				from teams as ht
+				inner join matches as m on ht.team_id = m.team_id1
+				group by name
+				union all
+				select
+				at.team_name_short as name,
+				sum(m.points_2) as points,
+				count(*) as games_played
+				from teams as at
+				inner join matches as m on at.team_id = m.team_id2
+				group by name)
+				select name, sum(points) as points, sum(games_played) as games_played, ((? - 1) * 2 - sum(games_played)) as games_left
+				from points_table
+				group by name
+				order by points desc
 			");
-		$sth->execute();
+		$sth->execute(array($teams_quantity));
 		$teams_names_by_pts = $sth->fetchAll();
-		var_dump($teams_names_by_pts);
-		/* */
+		/* match results */
 		$sth = $db->prepare("
-				select 
-				ft.team_name_short as home_team, 
-				st.team_name_short as away_team,
-				m.sets_won1 as home_sets_won,
-				m.sets_won2 as away_sets_won
-				from matches as m 
-				inner join teams as ft on m.team_id1 = ft.team_id
-				inner join teams as st on m.team_id2 = st.team_id
+				select
+				ht.team_name_short as team,
+				at.team_name_short as opponent,
+				(m.sets_won1 || ':' || m.sets_won2) as score
+				from matches as m
+				inner join teams as ht on ht.team_id = m.team_id1
+				inner join teams as at on at.team_id = m.team_id2
 			");
 		$sth->execute();
 		$results = $sth->fetchAll();
-		var_dump($results);
 		echo '<table>';
 		echo '<thead>';
 		echo '<tr>';
 		echo '<th>Команда</th>';
-		// for ($i = 0; $i < $teams_quantity; $i++)
-		// 	echo '<th сolspan="2">' . $teams[$i]['team_name_short'] . '</th>';
+		for ($i = 0; $i < $teams_quantity; $i++)
+			echo '<th сolspan="2">' . $teams_names_by_pts[$i]['name'] . '</th>';
 		echo '<th>Очки</th>';
 		echo '<th>Игры</th>';
 		echo '<th>Осталось</th>';
 		echo '</tr>';
 		$cols = $teams_quantity + 4;
-		// for ($i = 0; $i < $teams_quantity; $i++) {
-		// 	$home_team = $results[$i]['home_team'];
-		// 	echo '<tr>';
-		// 	for ($j = 0; $j < $cols; $j++)
-		// 	{
-		// 		switch ($j)
-		// 		{
-		// 			case 0:
-		// 				echo '<td>' . $home_team . '</td>';
-		// 				break ;
-		// 			case ($i + 1):
-		// 				echo '<td style="background-color:gray;"></td>';
-		// 				break ;
-		// 			default:
-		// 				echo '<td></td>';
-		// 				break ;
-		// 		}
-				
-		// 	}
-		// 	echo '</tr>';
-		// }
-		$db->query("update tournaments set tournament_description = 'Москва - 2 дивизион'");
+		for ($i = 0; $i < $teams_quantity; $i++) {
+			echo '<tr>';
+			for ($j = 0; $j < $cols; $j++)
+			{
+				$home_team = $teams_names_by_pts[$i]['name'];
+				switch ($j)
+				{
+					case 0:
+						echo '<td>' . $home_team . '</td>';
+						break ;
+					case ($i + 1):
+						echo '<td style="background-color:gray;"></td>';
+						break ;
+					case ($cols - 3):
+						echo '<td>' . $teams_names_by_pts[$i]['points'] . '</td>';
+						break ;
+					case ($cols - 2):
+						echo '<td>' . $teams_names_by_pts[$i]['games_played'] . '</td>';
+						break ;
+					case ($cols - 1):
+						echo '<td>' . $teams_names_by_pts[$i]['games_left'] . '</td>';
+						break ;
+					default:
+						$opponent = $teams_names_by_pts[$j - 1]['name'];
+						echo '<td>' . find_match_result($home_team, $opponent) . ' ' . find_match_result($opponent, $home_team) . '</td>';
+						break ;
+				}
+			}
+			echo '</tr>';
+		}
+		$db->query("update matches set team_id1 = 5, team_id2 = 1 where match_id = 49");
 
 		/* Utility function to display database tables */
 		function print_table($sql) {
@@ -105,6 +129,13 @@
 				echo '</tr>';
 			}
 			echo '</table>';
+		}
+		/* find match results in 2-dimensional array */
+		function find_match_result($team1, $team2) {
+			global $results;
+			foreach ($results as $result)
+				if ($result['team'] == $team1 && $result['opponent'] == $team2)
+					return ($result['score']);
 		}
 	?>
 </body>
