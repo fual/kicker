06446752d58f925c6810598290204d03a8b16450
Aleksander Antropov <aleksander.antropov@gmail.com>
2019-01-14 20:27:02 +0200

    Update functions

--- functions.php
+++ functions.php
@@ -22,8 +22,72 @@ function print_table($sql) {
 	echo '</table>';
 }
 /* print standings */
-function print_result_table() {
-	global $tournament, $standings;
+function print_result_table($division, $season) {
+	global $db;
+	/* teams quantity in the given $season and $division */
+	$sth = $db->prepare("
+			select
+			count(tms.team_name_short) as teams_quantity,
+			trn.tournament_rounds as rounds,
+			trn.tournament_id as tournament_id,
+			s.season_id as season_id
+			from teams as tms
+			inner join tournaments as trn on trn.tournament_id = tms.tournament_id
+			inner join seasons as s on tms.season_id = s.season_id
+			where trn.tournament_name = ? and s.season_name = ?
+		");
+	$sth->execute(array($division, $season));
+	$tournament = $sth->fetch();
+	/* teams names ordered by points scored */
+	$sql = "
+			with points_table as
+			(select
+			ht.team_name_short as name,
+			sum(m.points_1) as points,
+			count(*) as games_played
+			from teams as ht
+			inner join matches as m on ht.team_id = m.team_id1
+			where m.tournament_id = :tournament_id and m.season_id = :season_id
+			group by name
+			union all
+			select
+			at.team_name_short as name,
+			sum(m.points_2) as points,
+			count(*) as games_played
+			from teams as at
+			inner join matches as m on at.team_id = m.team_id2
+			where m.tournament_id = :tournament_id and m.season_id = :season_id
+			group by name)
+			select name, sum(points) as points, sum(games_played) as games_played, ((:qty - 1) * :r - sum(games_played)) as games_left
+			from points_table
+			group by name
+			order by points desc";
+	$sth = $db->prepare($sql);
+	$sth->bindValue(':tournament_id', $tournament['tournament_id'], PDO::PARAM_INT);
+	$sth->bindValue(':season_id', $tournament['season_id'], PDO::PARAM_INT);
+	$sth->bindValue(':qty', $tournament['teams_quantity'], PDO::PARAM_INT);
+	$sth->bindValue(':r', $tournament['rounds'], PDO::PARAM_INT);
+	$sth->execute();
+	$standings = $sth->fetchAll();
+	$sth = $db->prepare("
+			select
+			m.match_id as match_id,
+			m.season_id as season_id,
+			m.tournament_id as tournament_id,
+			ht.team_name_short as home_team,
+			at.team_name_short as away_team,
+			m.sets_won1 as home_score,
+			m.sets_won2 as away_score
+			from matches as m
+			inner join teams as ht on ht.team_id = m.team_id1
+			inner join teams as at on at.team_id = m.team_id2
+			where m.season_id = :season_id and m.tournament_id = :tournament_id
+		");
+	$sth->bindValue(':tournament_id', $tournament['tournament_id'], PDO::PARAM_INT);
+	$sth->bindValue(':season_id', $tournament['season_id'], PDO::PARAM_INT);
+	$sth->execute();
+	// $sth->execute(array(1, 1));
+	$results = $sth->fetchAll();
 	echo '<table>';
 	echo '<thead>';
 	echo '<tr>';
@@ -59,7 +123,11 @@ function print_result_table() {
 					break ;
 				default:
 					$away_team = $standings[$j - 1]['name'];
-					echo '<td>' . find_match_results($home_team, $away_team) . '</td>';
+					$mr = find_match_results($home_team, $away_team, $results);
+					foreach ($mr as $r)
+					{
+						echo '<td>' .  $r . '</td>';
+					}
 					break ;
 			}
 		}
@@ -67,21 +135,21 @@ function print_result_table() {
 	}
 }
 /* find match results in 2-dimensional array */
-function find_match_results($home_team, $away_team) {
-	global $results;
-	$res = " ";
+function find_match_results($home_team, $away_team, $results) {
+	$res = [];
 	foreach ($results as $result)
 		if ($result['home_team'] == $home_team && $result['away_team'] == $away_team)
 		{
-			$res .= '<a href="match?id=' . $result['match_id'] . '">';
-			$res .= $result['home_score'] . ':' . $result['away_score'];
-			$res .= '</a>' . " ";
+			$r = '<a href="match?id=' . $result['match_id'] . '">';
+			$r .= $result['home_score'] . ':' . $result['away_score'];
+			$r .= '</a>' . " ";
 		}
 		else if ($result['away_team'] == $home_team && $result['home_team'] == $away_team)
 		{
-			$res .= '<a href="match?id=' . $result['match_id'] . '">';
-			$res .= $result['away_score'] . ':' . $result['home_score'];
-			$res .= '</a>' . " ";
+			$r = '<a href="match?id=' . $result['match_id'] . '">';
+			$r .= $result['away_score'] . ':' . $result['home_score'];
+			$r .= '</a>' . " ";
 		}
+		array_push($res, $r);
 	return ($res);
 }
--- index.php
+++ index.php
@@ -6,73 +6,9 @@
 <body>
 	<?php
 		require_once 'autoload.php';
-		$division = "mos2";
-		$season = "2018";	
-		/* teams quantity in the given $season and $division */
-		$sth = $db->prepare("
-				select
-				count(tms.team_name_short) as teams_quantity,
-				trn.tournament_rounds as rounds,
-				trn.tournament_id as tournament_id,
-				s.season_id as season_id
-				from teams as tms
-				inner join tournaments as trn on trn.tournament_id = tms.tournament_id
-				inner join seasons as s on tms.season_id = s.season_id
-				where trn.tournament_name = ? and s.season_name = ?
-			");
-		$sth->execute(array($division, $season));
-		$tournament = $sth->fetch();
-		var_dump($tournament['tournament_id']);
-		/* teams names ordered by points scored */
-		$sth = $db->prepare("
-				with points_table as
-				(select
-				ht.team_name_short as name,
-				sum(m.points_1) as points,
-				count(*) as games_played
-				from teams as ht
-				inner join matches as m on ht.team_id = m.team_id1
-				where m.tournament_id = ?
-				group by name
-				union all
-				select
-				at.team_name_short as name,
-				sum(m.points_2) as points,
-				count(*) as games_played
-				from teams as at
-				inner join matches as m on at.team_id = m.team_id2
-				where m.tournament_id = ?
-				group by name)
-				select name, sum(points) as points, sum(games_played) as games_played, ((? - 1) * ? - sum(games_played)) as games_left
-				from points_table
-				group by name
-				order by points desc
-			");
-		$sth->execute(
-			array(
-				$tournament['tournament_id'],
-				$tournament['tournament_id'],
-				$tournament['teams_quantity'],
-				$tournament['rounds']
-			)
-		);
-		$standings = $sth->fetchAll();
-		var_dump($standings);
-		$sth = $db->prepare("
-				select
-				m.match_id as match_id,
-				ht.team_name_short as home_team,
-				at.team_name_short as away_team,
-				m.sets_won1 as home_score,
-				m.sets_won2 as away_score
-				from matches as m
-				inner join teams as ht on ht.team_id = m.team_id1
-				inner join teams as at on at.team_id = m.team_id2
-			");
-		$sth->execute();
-		$results = $sth->fetchAll();
 		/* match results */
-		// print_result_table();
+		print_result_table("mos2", "2018");
+		print_result_table("mos1", "2018");
 		// print_table("select * from teams");
 		// print_table("select * from tournaments");
 		// print_table("select * from matches");
