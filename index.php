5d29b34e6016c8a8aaf8c0ca480f354604a341a6
Aleksander Antropov <aleksander.antropov@gmail.com>
2019-03-09 17:40:43 +0300

    Update index

--- index.php
+++ index.php
@@ -1,37 +1,4 @@
 <?php
-	/* match results */
-	// print_table("select * from teams where tournament_id = 3");
-	// print_table("select * from matches where tournament_id = 2");
-	// print_table("select * from teams");
-	// print_table("select * from players");
-	// print_table("select * from rosters");
-	// print_result_table("mos2", "2018");
-	// print_result_table("mos1", "2018");
-	// print_result_table("mos3", "2018");
-	// print_table("select * from tournaments");
-	// print_table("select * from matches");
-	// $test = $db->query("
-	// 	select
-	// 	ht.team_name_short as name,
-	// 	sum(m.points_1) as points,
-	// 	count(*) as games_played
-	// 	from matches as m
-	// 	inner join teams as ht on ht.team_id = m.team_id1
-	// 	where m.tournament_id = 2 and m.season_id = 1
-	// 	group by name");
-	// foreach ($test as $t)
-	// {
-	// 	foreach ($t as $k => $v)
-	// 		echo $k . ' => ' . $v . '<br>';
-	// 	echo '<br>';
-	// }
-	// $db->query("update teams set team_name_long = 'Работяги 2' where team_id = 6");
-	// $db->query("update teams set team_name_long = 'Подзабей' where team_id = 7");
-	// $db->query("update teams set team_name_long = 'Научный подход' where team_id = 8");
-	// $db->query("update teams set team_name_long = 'Забей' where team_id = 10");
-	// $db->query("update teams set team_name_long = 'Работяги' where team_id = 13");
-?>
-<?php
 	require_once __DIR__ . "/inc/bootstrap.php";
 	require_once __DIR__ . "/inc/layout/head.php";
 	$db->query("update teams set team_name_long = 'ЗАБЕЙ' where team_id = 29");
@@ -46,7 +13,7 @@
 <main role="main" class="container">
   	<div class="starter-template pt-0">
     	<h2 class="mb-3">Добавить результат</h2>
-    	<form method="post" action="procedures/addResult.php">
+    	<form method="post" action="">
     		<div class="form-row">
     			<div class="col">
 		    		<select class="custom-select" id="inputTournament" name="division" required>
@@ -99,6 +66,7 @@
   		<h2 class="mt-4 mb-3">Второй дивизион</h2>
     	<?php print_result_table(5, "2019"); ?>
     	<p class="small mt-5">Если результат какого-либо матча отображен неверно, свяжитесь с <a href="http://vk.com/aantropov">нами</a>.</p>
+    	<p class="small mt-5">Upcoming updates: редактирование счета, расписание предстоящих игр с площадками, рейтинг игроков, заполнение протокола онлайн, личные кабинеты для капитанов, управление ростером.</p>
   	</div>
 </main>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
@@ -113,6 +81,14 @@
 			$("#inputTeam1 option, #inputTeam2 option").removeAttr("style");
 			$("#inputTeam1 [data-tournament!=" + tournament + "], #inputTeam2 [data-tournament!=" + tournament + "]").hide();
 		}
+		$("form").submit(function() {
+			var data = $(this).serialize();
+			$.ajax({
+				type: "POST",
+				url: "procedures/addResult.php",
+				data: data
+			});
+		});
 	});
 </script>
 <?php require_once __DIR__ . "/inc/layout/footer.php"; ?>
