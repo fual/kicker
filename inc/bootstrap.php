42a9c3191e4cb08346587cd1601f89feada24039
Aleksander Antropov <aleksander.antropov@gmail.com>
2019-03-12 17:49:34 +0300

    delete vendor + unnecessary code

--- inc/bootstrap.php
+++ inc/bootstrap.php
@@ -1,10 +1,3 @@
 <?php
-require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/database/connection.php';
 require_once __DIR__ . '/functions.php';
-
-$dotenv = Dotenv\Dotenv::create(__DIR__);
-$dotenv->load();
-
-$session = new \Symfony\Component\HttpFoundation\Session\Session();
-$session->start();
