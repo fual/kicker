7e983d224a44f9a61d3a5c3c969cc1652699c5f1
Aleksander Antropov <aleksander.antropov@gmail.com>
2019-03-24 13:10:19 +0300

    Update scripts.php, styles.php

--- inc/layout/styles.php
+++ inc/layout/styles.php
@@ -1,3 +1,3 @@
 <link rel="stylesheet" href="inc/libs/bootstrap/css/bootstrap.min.css">
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
-<link rel="stylesheet" href="inc/css/custom.css?v=4">
+<link rel="stylesheet" href="inc/css/custom.css?v=5">
