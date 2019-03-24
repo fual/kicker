000a50cb0ca47a56191d46bb1692c2f3e2d94385
Aleksander Antropov <aleksander.antropov@gmail.com>
2019-03-24 12:42:55 +0300

    Update scripts version

--- inc/layout/scripts.php
+++ inc/layout/scripts.php
@@ -1,3 +1,3 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js" defer></script>
 <script src="inc/libs/bootstrap/js/bootstrap.min.js" defer></script>
-<script src="inc/js/main.js?v=4" defer></script>
+<script src="inc/js/main.js?v=5" defer></script>
