5215a3e634d16f488a74b871348904e8949facf2
Aleksander Antropov <aleksander.antropov@gmail.com>
2019-03-26 11:55:25 +0300

    Update input_score.php

--- inc/layout/templates/input_score.php
+++ inc/layout/templates/input_score.php
@@ -1,4 +1,4 @@
-<form method="post" action="procedures/addResult" id="addResult" class="mt-4">
+<form method="post" action="procedures/addResult.php" id="addResult" class="mt-4">
 	<input type="hidden" name="tournament_id" value="<?php echo $tournament_id;?>">
 	<input type="hidden" name="team1_id" value="<?php echo $team1_id;?>">
 	<input type="hidden" name="team2_id" value="<?php echo $team2_id;?>">
