dc6feb94e34e82a063d161aab208a835fa4d2051
Aleksander Antropov <aleksander.antropov@gmail.com>
2019-03-29 18:06:51 +0300

    update scripts, styles

--- inc/layout/scripts.php
+++ inc/layout/scripts.php
@@ -2,4 +2,4 @@
 <script src="inc/libs/bootstrap/js/bootstrap.min.js" defer></script>
 <script src="inc/libs/jquery-ui/jquery-ui.min.js" defer></script>
 <script src="inc/libs/jquery-ui/jquery.ui.datepicker-ru.js" defer></script>
-<script src="inc/js/main.js?v=8" defer></script>
+<script src="inc/js/main.js?v=9" defer></script>
